9e8fbc7f9e41c7281d0f7837e848a85183361d2b
Karolina Kopecka <kopecka.karolina@gmail.com>
2024-10-07 18:59:41 +0200

    novy

---
+++ script.js
@@ -0,0 +1,6 @@
+const jmeno = prompt ("Zadej jméno prvního očkovaného")
+const vek = Number(prompt ("Zadej věk prrvního očkovaného:"))
+
+//document.body.innerHTML = "<p> " + jmeno1 + ", " + vek1 + " </p>"
+document.body.innerHTML = `<p> ${jmeno} ${vek} </p>`
+
